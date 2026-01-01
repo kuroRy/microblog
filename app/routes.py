4edaad9ffbca9887ec2845a047a47cbb3828e628
kanChome <hellbird1998@gmail.com>
2026-01-01 20:53:34 +0900

    add: ログイン機能の実装

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,17 @@
+from flask_login import login_user, current_user, logout_user, login_required
 from flask import render_template, flash, redirect, url_for
+import sqlalchemy as sa
 from app import app
 from app.forms import LoginForm
+from app.models import User
+from app import db
+from flask import request
+from urllib.parse import urlsplit
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-  user = {'username': 'John Doe'}
   posts = [
     {
       'author': {'username': 'John'},
@@ -16,12 +22,26 @@ def index():
       'body': 'The Avengers movie was so cool!'
     }
   ]
-  return render_template('index.html', title='Home', user=user, posts=posts)
+  return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
   form = LoginForm()
   if form.validate_on_submit():
-    flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-    return redirect(url_for('index'))
+    user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
+    if user is None or not user.check_password(form.password.data):
+      flash('Invalid username or password')
+      return redirect(url_for('login'))
+    login_user(user, remember=form.remember_me.data)
+    next_page = request.args.get('next')
+    if not next_page or urlsplit(next_page).netloc != '':
+      next_page = url_for('index')
+    return redirect(next_page)
   return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+  logout_user()
+  return redirect(url_for('index'))
